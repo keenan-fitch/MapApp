21b0ed52719bd751ad104eba5cfe89c227eb0b0c
keenan-fitch <21296537@student.uwa.edu.au>
2021-08-07 11:26:06 +0800

    Intial Commit

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
+// Launch App: 
+// Make sure in Desktop/Apps/MappApp directory
+//  $ npm start
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  Dimensions,
+  StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  SafeAreaView, 
+  Button,
+  Platform,
+  TouchableOpacity} from 'react-native';
+  //import { useDimensions } from '@react-native-community/hooks';
+  
+  function padding(a, b, c, d) {
+    return {
+      paddingTop: a,
+      paddingRight: b ? b : a,
+      paddingBottom: c ? c : a,
+      paddingLeft: d ? d : (b ? b : a)
+    }
+  }
+  export default function App() {
 
-export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <View style={{
+          backgroundColor: 'dodgerblue',
+          width: "100%",
+          height: "30%",
+        }}
+      ></View> */}
+     
+      <Text style={{...padding(10, 20, 40, 20), color: "black", fontSize: 20, fontFamily: 'Verdana', fontWeight: 'bold'}}>This is a Map in a App!</Text>
+      <TouchableOpacity onPress={() => console.log("Image tapped")}>
+        <Image source={{
+          width: 150,
+          height: 200,
+          uri: "https://d33wubrfki0l68.cloudfront.net/1484aa80faa12a0025d69af681b6428147f1104f/5a9c3/assets/logos/uwa.png"
+        }} />
+      </TouchableOpacity>
+    
+    
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +53,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    fontFamily: "Verdana",
+    fontSize: 14,
     alignItems: 'center',
     justifyContent: 'center',
   },
