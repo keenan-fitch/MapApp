1272a677ae63c97a86f3d578e0b75e3fad9fe16b
wjy9902 <49051666+wjy9902@users.noreply.github.com>
2021-08-31 08:21:19 +0800

    update readme

---
+++ firebase/README.md
@@ -0,0 +1,35 @@
+## Some softwares / packages are required to download:
+
++ Firebase console
+https://console.firebase.google.com
+
++ VS Code:
+https://code.visualstudio.com/download
+
++ nodeJS:
+Must have version 12 or higher.
+
+### Set up sdk
++ Download Firebase ommand tool
+```
+  $ sudo npm install -g firebase-tools
+```
+
++ Firebase login
+```
+  $ firebase login
+```
+
++ Install sdk for admin
+```
+  $ npm install firebase-admin --save
+```
+
++ Install uuid to easily link to storage
+```
+  $ npm install uuid-v4 --save
+```
+### Run main.js
+```
+  $ node main.js
+```
--- firebase/main.js
+++ firebase/main.js
@@ -1,10 +1,9 @@
-var admin = require('firebase-admin');
-
-// obtain key certification documents
+var uuid = require('uuid-v4');
+var admin = require("firebase-admin");
 var serviceAccount = require("./mapapp-c5b1e-firebase-adminsdk-d80s8-5323b48a37.json");
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	storageBucket: "gs://mapapp-c5b1e.appspot.com"
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "gs://mapapp-c5b1e.appspot.com"
 });
 var bucket = admin.storage().bucket();
 
@@ -13,16 +12,3 @@ let db = admin.firestore();
 
 // get timestamp
 const FieldValue = admin.firestore.FieldValue;
-
-async function downloadFile(srcFilename, destFilename) {
-    const options = {
-        // The path to which the file should be downloaded, e.g. "./file.txt"
-        destination: destFilename,
-    };
-    // Downloads the file
-    await bucket.file(srcFilename).download(options);
-    console.log(
-        `${srcFilename} downloaded to ${destFilename}.`
-    );
-}
-downloadFile('WechatIMG74.jpeg', 'WechatIMG74.jpeg').catch(console.error);
