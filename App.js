515ebf977d413b0d3ce2177baea08540af8649c2
benha2941 <bennyha20@gmail.com>
2021-09-09 21:30:30 +0800

    Firebase implemntation
    Able to put in data into the firebase

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
-import 'react-native-gesture-handler';
-import React, {useState} from 'react';
+import "react-native-gesture-handler";
+import React, { useState } from "react";
 import Home from "./app/screens/HomeScreen";
-import { View } from 'react-native';
-import Navigator from './routes/homeStack'
+import { View } from "react-native";
+import Navigator from "./routes/homeStack";
 //import { StackNavigator } from 'react-navigation';
 //import WelcomeScreen from './app/screens/WelcomeScreen';
 //import MapScreen from './app/screens/MapScreen';
 //import NavigationScreen from './app/screens/NavigationScreen';
 //import SettingsScreen from './app/screens/SettingsScreen';
+import * as firebase from "firebase";
+import "firebase/firestore";
 
- 
+const firebaseConfig = {
+  apiKey: "AIzaSyDXyXraHgu5hZW89RiJCd5MxcR1Ct3HAK4",
+  authDomain: "uwamap-cbeb4.firebaseapp.com",
+  projectId: "uwamap-cbeb4",
+  storageBucket: "uwamap-cbeb4.appspot.com",
+  messagingSenderId: "379875741857",
+  appId: "1:379875741857:web:1a621c346fc873ff4760e0",
+  measurementId: "G-08Y6D94TDZ",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const dbh = firebase.firestore();
+
+dbh.collection("characters").doc("mario").set({
+  employment: "plumber",
+  outfitColor: "red",
+  specialAttack: "fireball",
+});
+
 // export default function App() {
 //   return <WelcomeScreen />;
 
 export default function App() {
   return <Navigator />;
 }
-
 
 // export default class App extends Component {
 //   render() {
