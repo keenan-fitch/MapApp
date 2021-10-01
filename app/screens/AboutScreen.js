637888480a70ae0f2d5c51715b3b92fe4677d2ba
JLhyun <79490049+JLhyun@users.noreply.github.com>
2021-10-01 11:06:28 +0800

    Updated the About Page

--- app/screens/AboutScreen.js
+++ app/screens/AboutScreen.js
@@ -1,49 +1,61 @@
 import React from 'react';
-import { Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View, ScrollView,} from 'react-native';
+
 
 function About(props) {
     return (
+        
         <ImageBackground style={styles.background} source={require('../assets/#ececec.png')}>
-            <ScrollView style={styles.scollV}>
-
+            <ScrollView style={styles.scrollV}>
                 <Text style={styles.aboutTitle}> About the UWHS</Text>
 
                 <Image style={styles.logo} source={{uri: "https://crowdresearch.uwa.edu.au/wp-content/uploads/2015/12/cropped-uwa-crest-512.png",}}>
                 </Image>
                 <Text style={styles.aboutBody}>
-                    The UWA Historical Society commenced in 2008 from the initiatives of a small group led by Prof Reg Appleyard and the then Heritage Officer, Shobha Cameron. It received founding financial assistance from the Convocation of UWA graduates, and the support of Vice-Chancellor Prof. Alan Robson. The first event was a public lecture by Robert French, the Chief Justice of the High Court of Australia. He recalled his ‘Halycon Days‘ as a student at UWA. 
-                </Text>
-                <Text></Text> 
-                <Text></Text> 
+                    The UWA Historical Society commenced in 2008 from the initiatives of a small group led by Prof Reg Appleyard and the then Heritage Officer, Shobha Cameron. It received founding financial assistance from the Convocation of UWA graduates, and the support of Vice-Chancellor Prof. Alan Robson. The first event was a public lecture by Robert French, the Chief Justice of the High Court of Australia.
+                </Text><Text></Text>
                 <Text style={styles.aboutBody}>
                     Further distinguished graduates and staff offered Annual Lectures in the lead-up to the University Centenary celebrations. The Society has conducted seminars on ‘The Founding Professors’ and numerous ‘walking tours’ of the historical architectural features of the Crawley Campus, together with associated publications. 
-                </Text><Text></Text><Text></Text> 
-                <Text style={styles.aboutBody}>
-                    An extensive professional quality Oral History program has been vigorously pursued. A number of initiatives such as an annual ceremony to mark Remembrance Day, in conjunction with the University Regiment, the renewal of the WW1 Honour Board , and an Online WW2 Nominal Roll have received modest but helpful Grants.
                 </Text><Text></Text>
+                <Text style={styles.aboutBody}>
+                    An extensive professional quality Oral History program has been vigorously pursued. A number of initiatives such as an annual ceremony to mark Remembrance Day, in conjunction with the University Regiment, the renewal of the WW1 Honour Board.
+                </Text>
                 <Image style={styles.imageW} source={require("../assets/winthrop.jpeg")} ></Image>
-                
-                <Text></Text> 
+                <Text></Text><Text></Text>
                 <Text style={styles.aboutBody}>
                     From small leaflets commemorating the Old Dolphin Theatre, Anniversaries of the Sunken Garden Amphitheatre, the Octagon and New Fortune theatres, more substantial publications have been created by the small group of volunteers. ‘Personalities and Places on the Crawley Campus’ has already needed a reprint.
-                </Text><Text></Text><Text></Text> 
-
-                <Text style={styles.aboutBody}>
-                    The University Archivist is an ex-officio member of the annually elected working Committee. The membership fee is modest and open to all interested in the Society’s aims.
-                </Text><Text></Text><Text></Text> 
-                <Text style={styles.aboutBody}>
-                    Please check the website for further information. 
                 </Text><Text></Text>
                 <Text style={styles.aboutBody}>
+                    The University Archivist is an ex-officio member of the annually elected working Committee. The membership fee is modest and open to all interested in the Society’s aims.
+                </Text><Text></Text>
+                <Text style={styles.aboutBody}>
+                    Please check the website for further information. 
+                </Text>
+                <Text style={styles.aboutBody}>
                     https://www.web.uwa.edu.au/uwahs
-                </Text><Text></Text><Text></Text><Text></Text><Text></Text><Text></Text>
-
-
-
-
-
-
-
+                </Text><Text></Text>
+                <Text style={styles.aboutBody}>
+                Developed by Group 36.
+                </Text><Text></Text>                 
+                <Text style={styles.aboutBody}>
+                    Benny Ha - email@email.com {"\n"}
+                    Jess Gugliotta- email@email.com {"\n"}
+                    Jiayu Wu - email@email.com {"\n"}
+                    Jordan Lee - email@email.com {"\n"}
+                    Keenan Fitch - email@email.com {"\n"}
+                </Text>
+                <Text style={styles.aboutBody}>
+                    Jess - email@email.com
+                </Text>
+                <Text style={styles.aboutBody}>
+                    Jordan - email@email.com
+                </Text>
+                <Text style={styles.aboutBody}>
+                    
+                </Text>
+                <Text style={styles.aboutBody}>
+                    
+                </Text>
             </ScrollView>
         </ImageBackground>   
     );
@@ -56,7 +68,7 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "center",
     },
-    loginButton: {
+    loginButton: { //Where is the login button?
         width: "100%",
         height: 70,
         backgroundColor: "#ececec",
@@ -94,7 +106,7 @@ const styles = StyleSheet.create({
         width: "100%",
         alignSelf: "center"
     },
-    signup: {
+    signup: { //Can we remove this
         backgroundColor: 'white',
         color: '#3A59FF',
         width: "65%",
@@ -121,10 +133,10 @@ const styles = StyleSheet.create({
 
     aboutBody: {
         width: "80%",
-        top: 90,
+        top: 80,
         borderRadius: 25,
         alignSelf: "center",
-        fontSize:  18,
+        fontSize:  14,
         position: "relative"
 
     },
@@ -133,16 +145,18 @@ const styles = StyleSheet.create({
         top: 40,
         borderRadius: 25,
         alignSelf: "center",
-        fontSize:  32,
+        fontSize:  30,
         position: "relative",
         textAlign: "center",
         fontFamily: "Futura",
        // fontWeight: "bold",
         // letterSpacing: 10,
     },
-    scollV: {
+    scrollV: {
+        flex: 1,
         width: "100%",
-        position: "relative"
+        position: "relative",
     }
 
 });
+ 
